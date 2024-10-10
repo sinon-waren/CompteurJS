d92b63cad3901077150e1db587acf70ebd1510da
sinon-waren <sinonwaren2000@gmail.com>
2024-10-10 12:16:50 +0200

    Update of the project

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
-let compteur = 0; 
+const counter = (function () {
 
-function cmpincrement (){
-    compteur++; 
-    document.getElementById("compteur").textContent = compteur; 
-}
+    let compteur = 0; 
 
-function decremente () {
-    compteur--;
-    document.getElementById("compteur").textContent = compteur;
-}
+    function updateDisplay () {
+        document.getElementById("compteur").textContent = compteur;
+    }
 
-function cptreset () {
-    compteur = 0;
-    document.getElementById("compteur").textContent = compteur;
-}
+    function incremente () {
+        compteur ++; 
+        updateDisplay();
+    }
 
-document.getElementById("incremente").addEventListener("click", cmpincrement); 
-document.getElementById("decremente").addEventListener("click", decremente); 
-document.getElementById("reset").addEventListener("click", cptreset); 
+    function decremente () {
+        compteur --; 
+        updateDisplay(); 
+    }
+
+    function reset () {
+        compteur = 0;
+        updateDisplay(); 
+    }
+
+    return {
+        incremente : incremente,
+        decremente : decremente,
+        reset : reset
+    }
+
+}) (); 
+
+document.getElementById("incremente").addEventListener("click", counter.incremente); 
+document.getElementById("decremente").addEventListener("click", counter.decremente); 
+document.getElementById("reset").addEventListener("click", counter.reset); 
